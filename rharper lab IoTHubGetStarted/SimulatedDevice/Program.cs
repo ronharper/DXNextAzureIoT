6c498c09825ea5816d5c77f908196683146e8103
ronharper <ronharper@outlook.com>
2017-03-28 15:01:55 -0700

    Initial Publish

---
+++ rharper lab IoTHubGetStarted/SimulatedDevice/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+
+namespace SimulatedDevice
+{
+    class Program
+    {
+        static DeviceClient deviceClient;
+        static string iotHubUri = "HostName=RHarper-Hub1.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=m7yDtsi0RQEb4vjAtIP0UXwXT7jiATDRmAsckuhxhzI=";
+        static string deviceKey = "BGi/cEOpXbQ9flmZwdmSLrjUnd1iWxvp9Dqanx3VuuE=";
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simulated device\n");
+            deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey("myFirstDevice", deviceKey), TransportType.Mqtt);
+
+            SendDeviceToCloudMessagesAsync();
+            Console.ReadLine();
+
+        }
+        private static async void SendDeviceToCloudMessagesAsync()
+        {
+            double avgWindSpeed = 10; // m/s
+            Random rand = new Random();
+
+            while (true)
+            {
+                double currentWindSpeed = avgWindSpeed + rand.NextDouble() * 4 - 2;
+
+                var telemetryDataPoint = new
+                {
+                    deviceId = "myFirstDevice",
+                    windSpeed = currentWindSpeed
+                };
+                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                var message = new Message(Encoding.ASCII.GetBytes(messageString));
+
+                await deviceClient.SendEventAsync(message);
+                Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+
+                Task.Delay(1000).Wait();
+            }
+        }
+
+    }
+}
